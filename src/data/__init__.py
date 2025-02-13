9ac9635fea3be592a5cffdf0b1358441c646f894
Gershom <gershom.seneviratne@gmail.com>
2025-02-12 19:37:30 -0500

    SCAND Dataloader fixed goal issues

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,4 @@
 from .nuscenes_pref_dataset import NuScenesPreferenceDataset
+from .scand_pref_dataset import SCANDPreferenceDataset
 
-__all__ = ["NuscenesPreferenceDataset","data"]
+__all__ = ["NuscenesPreferenceDataset", "SCANDPreferenceDataset", "data"]
